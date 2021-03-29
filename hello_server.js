b0408ec807a5d466afba23aa2ad2f64d31b22c28
Weslley Moura <weslley.data@gmail.com>
2021-03-29 00:55:46 -0300

    feat(project): working with routes

--- hello_server.js
+++ hello_server.js
@@ -2,7 +2,15 @@ var http = require('http')
 
 var server = http.createServer(function(req, res){
     res.writeHead(200, {"Content-Type": "text/html"})
-    res.write("Hello World!!!")
+    if (req.url == "/"){
+        res.write("<h1>Página principal</h1>")
+    }
+    else if (req.url == "/bemvindo"){
+        res.write("<h1>Bem vindo :</h1>")
+    }
+    else {
+        res.write("<h1>Página não encontrada</h1>")
+    }
     res.end()
 })
 
